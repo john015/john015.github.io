6de2198f0ecdbaa6b5c5680ebea4cc2ff1050444
john015 <lsw0150305@gmail.com>
2019-02-08 17:44:34 +0900

    Add: Post

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-c6121111d6df1841d543.js"
+    "url": "webpack-runtime-a21af6efd75915081d26.js"
   },
   {
     "url": "app-f6d02fdb276deff8e1d7.js"
@@ -36,7 +36,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "483387417a0dd1f9a12099b8e4ed6948"
+    "revision": "1b95758348ca9a304ae1547566915ebc"
   },
   {
     "url": "static/d/520/path---offline-plugin-app-shell-fallback-a-30-c5a-NZuapzHg3X9TaN1iIixfv1W23E.json"
