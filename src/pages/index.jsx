ced74e214a013ba24e8796ba576d02db0ba6df12
john015 <lsw0150305@gmail.com>
2019-07-29 22:14:32 +0900

    Add: infinity scroll feature | post

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,14 +1,54 @@
 import React from 'react'
 import Helmet from 'react-helmet'
 import { graphql } from 'gatsby'
+import throttle from 'lodash.throttle'
 import Layout from '../layout'
 import PostListing from '../components/PostListing'
 import SEO from '../components/SEO'
 import config from '../../data/SiteConfig'
 
 class Index extends React.Component {
+  state = { displayingData: [], dataLength: 0, isCompletedLoadingData: false }
+
+  onScroll = throttle(() => {
+    const { isCompletedLoadingData, displayingData, dataLength } = this.state
+    if (isCompletedLoadingData) return
+    const { data } = this.props
+    const postEdges = data.allMarkdownRemark.edges
+
+    const scrollTop =
+      (document.documentElement && document.documentElement.scrollTop) ||
+      document.body.scrollTop
+    const { innerHeight } = window
+    const { scrollHeight } = document.body
+
+    if (scrollHeight - innerHeight - scrollTop < 300) {
+      if (displayingData.length + 5 >= dataLength) {
+        this.setState({ isCompletedLoadingData: true })
+      }
+      this.setState({
+        displayingData: postEdges.slice(0, displayingData.length + 5)
+      })
+    }
+  }, 80)
+
+  componentDidMount() {
+    const { data } = this.props
+    const postEdges = data.allMarkdownRemark.edges
+    this.setState({
+      dataLength: postEdges.length,
+      displayingData: postEdges.slice(0, 5)
+    })
+    window.addEventListener('scroll', this.onScroll)
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('scroll', this.onScroll)
+  }
+
   render() {
     const { data, location } = this.props
+    const { displayingData } = this.state
     const postEdges = data.allMarkdownRemark.edges
     return (
       <Layout location={location} title="John`s Dev log">
@@ -18,7 +58,7 @@ class Index extends React.Component {
             <link rel="canonical" href={`${config.siteUrl}`} />
           </Helmet>
           <SEO postEdges={postEdges} />
-          <PostListing postEdges={postEdges} />
+          <PostListing postEdges={displayingData} />
         </div>
       </Layout>
     )
